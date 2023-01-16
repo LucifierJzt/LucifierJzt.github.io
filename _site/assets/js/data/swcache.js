30fa7b613446bc474d5e228f79131585e10c69f4
jinzhentao.troye <jinzhentao.troye@bytedance.com>
2023-01-16 17:57:54 +0800

    feat: bundle

---
+++ _site/assets/js/data/swcache.js
@@ -0,0 +1,60 @@
+const resource = [
+    /* --- CSS --- */
+    '/LucifierJzt.github.io/assets/css/style.css',
+
+    /* --- PWA --- */
+    '/LucifierJzt.github.io/app.js',
+    '/LucifierJzt.github.io/sw.js',
+
+    /* --- HTML --- */
+    '/LucifierJzt.github.io/index.html',
+    '/LucifierJzt.github.io/404.html',
+
+    
+        '/LucifierJzt.github.io/categories/',
+    
+        '/LucifierJzt.github.io/tags/',
+    
+        '/LucifierJzt.github.io/archives/',
+    
+        '/LucifierJzt.github.io/about/',
+    
+
+    /* --- Favicons & compressed JS --- */
+    
+    
+        '/LucifierJzt.github.io/assets/img/favicons/android-chrome-192x192.png',
+        '/LucifierJzt.github.io/assets/img/favicons/android-chrome-512x512.png',
+        '/LucifierJzt.github.io/assets/img/favicons/apple-touch-icon.png',
+        '/LucifierJzt.github.io/assets/img/favicons/favicon-16x16.png',
+        '/LucifierJzt.github.io/assets/img/favicons/favicon-32x32.png',
+        '/LucifierJzt.github.io/assets/img/favicons/favicon.ico',
+        '/LucifierJzt.github.io/assets/img/favicons/mstile-150x150.png',
+        '/LucifierJzt.github.io/assets/js/dist/categories.min.js',
+        '/LucifierJzt.github.io/assets/js/dist/commons.min.js',
+        '/LucifierJzt.github.io/assets/js/dist/home.min.js',
+        '/LucifierJzt.github.io/assets/js/dist/misc.min.js',
+        '/LucifierJzt.github.io/assets/js/dist/page.min.js',
+        '/LucifierJzt.github.io/assets/js/dist/post.min.js',
+        '/LucifierJzt.github.io/assets/js/dist/pvreport.min.js'
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+    
+
+    'localhost:4000',
+
+    
+
+    'fonts.gstatic.com',
+    'fonts.googleapis.com',
+    'cdn.jsdelivr.net',
+    'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+    
+];
+
